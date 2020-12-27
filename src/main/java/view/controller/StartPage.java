704c8ddf6a5ecef791d78ace0900fe4781b36403
romain325 <romainolivier42@gmail.com>
2020-12-27 11:30:45 +0100

    Add Intro Page

--- src/main/java/view/controller/StartPage.java
+++ src/main/java/view/controller/StartPage.java
@@ -1,29 +1,79 @@
 package main.java.view.controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import main.java.view.FRAME;
 import main.java.view.MainFrame;
 
 import java.net.URL;
+import java.sql.Time;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class StartPage implements Initializable {
     @FXML
+    private Button startEverything;
+    @FXML
+    private VBox firstText;
+    @FXML
+    private VBox secondText;
+    @FXML
     private Button changeScene;
     @FXML
-    private Label labelTxt;
+    private Button validName;
     @FXML
     private TextField fieldTxt;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        labelTxt.textProperty().bindBidirectional(fieldTxt.textProperty());
+        /*HIDING*/
+        hidePane(firstText);
+        hidePane(secondText);
+
+        /*BINDING*/
+        validName.visibleProperty().bind(fieldTxt.textProperty().isEmpty().not());
+        validName.textProperty().bindBidirectional(fieldTxt.textProperty());
+
+
+        /*BUTTON*/
         changeScene.setOnAction(actionEvent -> {
             MainFrame.switchScene(FRAME.OUTSIDE_MAIN, true);
         });
+        startEverything.setOnAction(actionEvent -> showTextProgressively(firstText));
+        validName.setOnAction(actionEvent -> showTextProgressively(secondText));
+    }
+
+    private void hidePane(Pane pane){
+        pane.setVisible(false);
+        for (var node: pane.getChildren()) {
+            node.setVisible(false);
+        }
+    }
+
+    private void showTextProgressively(Pane pane) {
+        if(pane.isVisible()) return;
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        pane.setVisible(true);
+        int it = 0;
+        for (var node : pane.getChildren()) {
+            it++;
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.millis(500*it),
+                            new KeyValue(node.visibleProperty(), true)
+                    ));
+        }
+        timeline.play();
     }
 }
